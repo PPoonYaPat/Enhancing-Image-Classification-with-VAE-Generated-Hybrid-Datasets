7899ba50aa762211d99c303e2458b96bd21cd4fe
PPoonYaPat <poonyapat4404@gmail.com>
2025-03-19 18:19:19 +0800

    Add function to calculate the probabilities of the randoms point in latent space

--- VAE.py
+++ VAE.py
@@ -7,7 +7,6 @@ from torch.optim.adam import Adam
 from torch.utils.data import DataLoader, TensorDataset
 import copy
 import time
-from collections import defaultdict
 
 class VAE(nn.Module):
 
@@ -20,6 +19,7 @@ class VAE(nn.Module):
         self.image_height = image_height
         self.input_dim = image_width * image_height * 3 # RGB-channel
         self.optimizer = optimizer
+        self.gamma = 1e-5
 
         # Encoder
         self.encoder = nn.Sequential(
@@ -84,7 +84,7 @@ class VAE(nn.Module):
     
     # Function to pre-calculating the output into latent space in order to calculate the likelihood distribution for random point (the below function)
     # input: Dataset
-    def dataset_latent_space(self, dataset):
+    def calculate_class_mu_var(self, dataset):
         self.eval()
         self.class_lists_mu = [[] for _ in range(self.class_num)]
         self.class_lists_var = [[] for _ in range(self.class_num)]
@@ -95,15 +95,13 @@ class VAE(nn.Module):
             for (image, label) in zip(images, labels):
                 with torch.no_grad():
                     mu, logvar = self.encode(image.view(-1, self.input_dim).to(self.device)).squeeze(0)
-                    var = torch.exp(logvar)
-                    self.class_lists_mu[label.item()].append(mu)
-                    self.class_lists_var[label.item()].append(var)
+                var = torch.exp(logvar)
+                self.class_lists_mu[label.item()].append(mu)
+                self.class_lists_var[label.item()].append(var)
         
         for idx in range(self.class_num):
-            mu = 0.0
-            for i in range(len(self.class_lists_mu[idx])):
-                mu += self.class_lists_mu[idx][i]
-            mu = mu / len(self.class_lists_mu[idx])
+
+            mu = self.class_lists_mu[idx].sum() / len(self.class_lists_mu[idx])
 
             var = 0.0
             for i in range(len(self.class_lists_var[idx])):
@@ -113,11 +111,37 @@ class VAE(nn.Module):
             self.class_mu[idx] = mu
             self.class_var[idx] = var
 
-    # Encoding random points into latent space has to be implemented
-    # input: Random points
+
+    # P(Normal(mu,var) = x)
+    def probs(self, x, mu, var):
+        normalized_x = (x - mu) / var
+        return torch.special.ndtr(normalized_x + self.gamma) - torch.special.ndtr(normalized_x - self.gamma)
+
+
+    # input: Random points in latent space -> [batch_size, 2, 2]
     # output: probability of the random points -> [batch_size, class_num(=100)]
     def probability(self, random_points):
-        return 0
+        probs = []
+        for _, (x, y) in random_points:
+            sum = 0.0
+            class_prob = []
+            for c in range(self.class_num):
+                mu_x = self.class_mu[c][0]
+                mu_y = self.class_mu[c][1]
+                var_x = self.class_var[c][0]
+                var_y = self.class_var[c][1]
+
+                # calculate p(z|c) = p((x,y)|c) = p(x|c) p(y|c)
+                prob_x = self.probs(x, mu_x, var_x)
+                prob_y = self.probs(y, mu_y, var_y)
+                class_prob.append(prob_x * prob_y)
+                sum += prob_x * prob_y
+
+            class_prob_normalized = [p / sum for p in class_prob]
+            probs.append(class_prob_normalized)
+
+        return torch.tensor(probs)
+        
     
 
     def train_model(self, dataloaders, epochs):
